40c057390accc02581c8514d99e489e48155c87a
nir659 <187752643+nir659@users.noreply.github.com>
2024-11-15 15:04:20 +1000

    Update entry.js

--- js/entry.js
+++ js/entry.js
@@ -1,9 +1,12 @@
 const enterScreen = document.getElementById('enter-screen');
 const enterButton = document.getElementById('enter-button');
 
-enterButton.addEventListener('click', () => {
+enterButton.addEventListener('click', hideEnterScreen);
+enterButton.addEventListener('touchstart', hideEnterScreen);
+
+function hideEnterScreen() {
     enterScreen.classList.add('hidden');
     setTimeout(() => {
         enterScreen.style.display = 'none';
     }, 500);
-});
+}
