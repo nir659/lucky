9c1238568bb5159974ba1a40436fb86e037ebec6
nir659 <187752643+nir659@users.noreply.github.com>
2024-11-15 15:04:07 +1000

    Update tyeffect.js

--- js/tyeffect.js
+++ js/tyeffect.js
@@ -1,4 +1,4 @@
-const phrases = ["follow me on spotify!", "minecraft pvp lord", "I wanna get haiiii !!!"];
+const phrases = [" follow my spotify! ", " minecraft pvp lord ", " I wanna get haiiii !!! "];
 const animatedText = document.querySelector(".animated-text span");
 
 let phraseIndex = 0;
@@ -7,35 +7,26 @@ let isDeleting = false;
 
 function type() {
     const currentPhrase = phrases[phraseIndex];
-    
-    // Update text content
     animatedText.textContent = currentPhrase.slice(0, charIndex);
-    
-    if (!isDeleting) {
-        animatedText.classList.add("typing"); // Start underline animation
-    } else {
-        animatedText.classList.remove("typing"); // Remove underline during deletion
-    }
 
     if (isDeleting) {
-        charIndex--; 
+        charIndex--;
     } else {
-        charIndex++; 
+        charIndex++;
     }
 
-    let speed = isDeleting ? 100 : 150;
+    let speed = isDeleting ? 150 : 200; // Slower speed for better mobile performance
 
     if (!isDeleting && charIndex === currentPhrase.length) {
-        speed = 700; 
+        speed = 1000;
         isDeleting = true;
-    } 
-    else if (isDeleting && charIndex === 0) {
+    } else if (isDeleting && charIndex === 0) {
         isDeleting = false;
-        phraseIndex = (phraseIndex + 1) % phrases.length; 
+        phraseIndex = (phraseIndex + 1) % phrases.length;
         speed = 500;
     }
 
     setTimeout(type, speed);
 }
 
-document.addEventListener("DOMContentLoaded", type);s
+document.addEventListener("DOMContentLoaded", type);
